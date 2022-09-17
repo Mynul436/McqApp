9724e3071803da80b8e85508c51484b66022673d
Mynul Islam <mynulakash436@gmail.com>
2022-09-17 09:50:20 +0600

    Componets

---
+++ mcq-client/src/components/Login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, TextField,Box, Card, CardContent, Typography } from "@mui/material";
+import Center from "./Center";
+
+export default function Login() {
+  return (
+    <Center>
+      <Card sx={{ width: 400 }}>
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h3" sx={{ my: 3 }}>
+            Quiz App
+          </Typography>
+          <Box
+            sx={{
+              "& .MuiTextField-root": {
+                m: 1,
+                width: "90%",
+              },
+            }}
+          >
+            <form noValidate>
+              <TextField
+                label="Email"
+                name="email"               
+                variant="outlined"
+               
+              />
+              <TextField
+                label="Name"
+                name="name"
+            
+                variant="outlined"
+                
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                sx={{ width: "90%" }}
+              >
+                Start
+              </Button>
+            </form>
+          </Box>
+        </CardContent>
+      </Card>
+    </Center>
+  );
+}
